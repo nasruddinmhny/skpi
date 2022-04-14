fb2a05e49eeb12418e2bf8ec09adb7e758152483
unknown <nasruddin@universitasmulia.ac.id>
2022-04-14 14:33:38 +0800

    update

--- surat_pendamping_ijazah/urls.py
+++ surat_pendamping_ijazah/urls.py
@@ -24,7 +24,7 @@ from django.views.static import serve
  
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/',include('skpi_management_app.urls')),
+    path('skpi/',include('skpi_management_app.urls')),
     #path('__debug__/', include('debug_toolbar.urls')),
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
