1ef7b68fd43d5c97acc23a12cd4d9947469e1092
unknown <nasruddin@universitasmulia.ac.id>
2022-04-14 12:48:08 +0800

    update heroku

--- surat_pendamping_ijazah/settings.py
+++ surat_pendamping_ijazah/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-3aogh8h+7zhikjc=$8wx4p^i#cpuzti0^v9t_5-a9v3nm#ju8-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','suratkpi.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1','suratkpi.herokuapp.com']
 
 
 # Application definition
