e2954f1f9ad0b1e9827da6372f9b05926d0fa7c5
unknown <nasruddin@universitasmulia.ac.id>
2022-04-14 12:19:59 +0800

    update heroku

--- surat_pendamping_ijazah/settings.py
+++ surat_pendamping_ijazah/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,6 +137,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = (BASE_DIR / 'media')
 MEDIA_URL = '/media/'
 
